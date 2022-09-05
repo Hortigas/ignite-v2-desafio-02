8bc512035e48f3b4fdfaa2b760f0e1f0d7c02359
Hortigas <xxvsvitor98xx@gmail.com>
2022-09-05 19:59:24 -0300

    basic structure
    folders and pages added

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+    max-width: 70rem;
+    padding: 2.5rem;
+    margin: 0 auto;
+
+    background-color:red;
+`;
