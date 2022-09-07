bfc2a0c3dc6f585af412e719026834b3c11a3d48
Hortigas <xxvsvitor98xx@gmail.com>
2022-09-07 15:00:32 -0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ignite-v2-desafio-02/',
   plugins: [react()]
-})
+});
